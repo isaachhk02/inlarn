e5fe8fa5ff8aec9aff3e3b04dd83833bbc76b21a
Isaac Ordoñez Fornós <isaac.fornos02@gmail.com>
2024-06-21 08:28:05 +0200

    Save

--- irlang.py
+++ irlang.py
@@ -1,1 +1,26 @@
+# Created by hhk02 (isaachhk02)
+import winreg
+import os
 
+zero = int(0)
+nada = None
+username = None
+password = None
+hrootMachine = winreg.HKEY_LOCAL_MACHINE
+
+def CreateUser(username: str, password: str):
+    print("Creating user...")
+    
+
+def start():
+    print("isaachhk02's Local Account Creator and Microsoft Account requirement bypass")
+    i = input("Press any key to start")
+    username = input("Username:")
+    if username != None:
+        if password != None:
+            CreateUser(username,password)
+        else:
+            start()
+        pass
+    pass
+pass
