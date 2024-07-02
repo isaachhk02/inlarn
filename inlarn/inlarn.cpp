0b55e9d1d418fd0db84be54d48da50a2ede37229
Isaac Ordoñez Fornós <isaac.fornos02@gmail.com>
2024-07-03 00:40:07 +0200

    Add files via upload

--- inlarn/inlarn.cpp
+++ inlarn/inlarn.cpp
@@ -3,16 +3,38 @@
 #include <iostream>
 #include <windows.h>
 #include <winreg.h>
-#include <process.h>
 #include "inlarn.h"
 #include <unistd.h>
 
 using namespace std;
 
-string username;
-string password;
+const char *username;
+const char *password;
 
-string lang;
+const char *user_add;
+
+const char *english;
+
+const char *german;
+
+const char *french;
+
+const char *spanish_portuguese;
+
+const char *italian;
+
+const char *chinese;
+
+
+const char *japanese;
+
+const char *russian;
+
+const char *arab;
+
+const char *hindi;
+
+char *lang;
 
 HKEY hKey;
 HKEY hKey2;
@@ -23,36 +45,56 @@ auto resultado_oobe = RegOpenKeyExA(HKEY_LOCAL_MACHINE, subkey_oobe, 0, KEY_WRIT
 auto open_winlogon = RegOpenKeyExA(HKEY_LOCAL_MACHINE, winlogon, 0, KEY_WRITE, &hKey);
 
 
-void CreateUser(string username, string password) {
-    cout << "Creating user\n";
-    execl("C:\\Windows\\System32\\net.exe",["user",username,password,"/add"]);
+void CreateUser() {
+    user_add = "user " , username  , password , "/add";
+
+    english = "localgroup Administrators " , username , "/add";
+
+    german = "localgroup Administratoren " , username , "/add";
+
+    french = "localgroup  Administrateurs " , username , "/add";
+
+    spanish_portuguese = "localgroup Administradores ",username,"/add";
+
+    italian = "localgroup Amministratori " , username , "/add";
+
+    chinese = "localgroup 管理员们 ",username, " /add";
+
+
+    japanese = "localgroup 管理者たち " , username , "/add";
+
+    russian = "localgroup Администраторы " , username , "/add";
+
+    arab = "localgroup مديرين " , username , "/add";
+
+    hindi  = "localgroup प्रशासकगण " , username , "/add";
+    printf("%s","Creating user....");
+    execl("C:\\Windows\\System32\\net.exe",user_add);
 
     
     if (lang == "english")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup"],["Administrators"],[username],["/add"]);
+        execl("C:\\Windows\\System32\\net.exe",english);
     if (lang == "german")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","Administratoren",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",german);
     if (lang == "french")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","Administrateurs",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",french);
     if (lang == "spanish" || lang == "portuguese")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","Administradores",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",spanish_portuguese);
     if (lang == "italian")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","Amministratori",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",italian);
     if (lang == "chinese")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","管理员们",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",chinese);
     if (lang == "japanese")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","管理者たち",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",japanese);
         
     if (lang == "russian")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","Администраторы",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",russian);
     if (lang == "arabic")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","مديرين",username,"/add"]);
+        execl("C:\\Windows\\System32\\net.exe",arab);
     if (lang == "hindi")
-        execl("C:\\Windows\\System32\\net.exe",["localgroup","प्रशासकगण",username,"/add"]);
-    cout << "User " << username.c_str() << " created" << " successfully!\n";
-    } else {
-        cout << "Error!";
-    }
+        execl("C:\\Windows\\System32\\net.exe",hindi);
+    printf("%s","User created successfully!\n");
+    
     
     
 
@@ -62,48 +104,48 @@ void PatchRegistry()
     const char* useroobe = "LaunchUserOOBE";
     cout << "Patching\n";
     if (resultado_oobe == ERROR_SUCCESS) {
-        RegDeleteValueA(hKey2, useroobe);
+        RegDeleteValueA(hKey2,useroobe);
     }
     if (open_winlogon == ERROR_SUCCESS) {
-        RegSetValueExA(hKey, "AutoAdminLogon", 0, REG_SZ, (const BYTE*)"0", 0);
-        RegSetValueExA(hKey, "AutoLogonSID", 0, REG_SZ, (const BYTE*)"", 0);
-        RegSetValueExA(hKey, "DefaultUserName", 0, REG_SZ, (const BYTE*)"", 0);
-        RegSetValueExA(hKey, "EnableFirstLogonAnimation", 0, REG_DWORD, (const BYTE*)(0), 0);
+        RegSetValueExA(hKey,"stringAdminLogon", 0, REG_SZ, (const BYTE*)"0",0);
+        RegSetValueExA(hKey,"stringLogonSID", 0, REG_SZ, (const BYTE*)"", 0);
+        RegSetValueExA(hKey,"DefaultUserName", 0, REG_SZ, (const BYTE*)"", 0);
+        RegSetValueExA(hKey,"EnableFirstLogonAnimation", 0, REG_DWORD, (const BYTE*)(0), 0);
 
-        cout << "All done!\n";
+        printf("%s","All done!\n");
         RegCloseKey(hKey);
     }
     else {
-        cout << "ERROR ";
+        printf("%s","ERROR\n");
     }
 }
 int main()
 {
-    std::cout << "isaachhk02s Local Account Creator and Microsoft Account requirement bypass\n";
-    system("pause");
-    cout << "Write your language:\n Available languages:\nENGLISH SPANISH FRENCH GERMAN ITALIAN PORTUGUESE CHINESE JAPANESE RUSSIAN ARABIC HINDI\n";
+    printf("%s","isaachhk02s Local Account Creator and Microsoft Account requirement bypass\n");
+    getchar();
+    printf("%s","Write your language:\n");
     
-    cin >> lang;
+    scanf("%s",lang);
         
         
         
-    cout << "Username:";
+    printf("%s","Write your username:");
 
-    cin >> username;
+    scanf("%s",username);
     if (username != "") {
-        cout << "Password:";
-        cin >> password;
-        cin >> lang;
+        printf("%s","Write your password:");
+        scanf("%s",password);
+        scanf("%s",lang);
         if (password == "")
         {
             password = "";
-            CreateUser(username.c_str(),password.c_str());
+            CreateUser();
             PatchRegistry();
             
             
         } 
         else {
-            CreateUser(username.c_str(),password.c_str());
+            CreateUser();
             PatchRegistry();
             
         }
--- inlarn/inlarn.h
+++ inlarn/inlarn.h
@@ -1,6 +1,5 @@
 #define INLARN_H
-#include <iostream>
-using namespace std;
+
 
 void PatchRegistry();
-void CreateUser(string username, string password);
+void CreateUser();
