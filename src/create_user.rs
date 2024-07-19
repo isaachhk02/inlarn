a9947a3ea6abba33472adb7f0a9562e1a86abfe4
isaachhk02 <isaac.fornos02@gmail.com>
2024-07-19 19:11:37 +0200

    Added crate netuser-rs and remove winapi crate

--- src/create_user.rs
+++ src/create_user.rs
@@ -1,60 +1,18 @@
 
-#[cfg(windows)] extern crate winapi;
-extern crate widestring;
+#[cfg(windows)] extern crate netuser_rs;
 
 #[cfg(windows)]
 pub fn createuser(_username : &str, _password: &str) {
-    use std::ptr::null_mut;
-    use widestring::U16CString;
-    use winapi::shared::lmcons::NET_API_STATUS;
-    use winapi::um::lmaccess::{NetUserAdd, NetLocalGroupAddMembers, USER_INFO_1, LOCALGROUP_MEMBERS_INFO_3, USER_PRIV_USER};
-
-    unsafe {
-        // Define the user details
-        let username = U16CString::from_str(_username).unwrap();
-        let password = U16CString::from_str(_password).unwrap();
-
-        // Prepare USER_INFO_1 structure
-        let user_info = USER_INFO_1 {
-            usri1_name: username.as_ptr() as *mut _,
-            usri1_password: password.as_ptr() as *mut _,
-            usri1_priv: USER_PRIV_USER,
-            usri1_home_dir: null_mut(),
-            usri1_comment: null_mut(),
-            usri1_flags: 0x0001, // UF_SCRIPT
-            usri1_script_path: null_mut(),
-            usri1_password_age: 0x0001,
-        };
-
-        let mut parm_err: u32 = 0;
-
-        // Add the user
-        let status: NET_API_STATUS = NetUserAdd(null_mut(), 1, &user_info as *const _ as *mut _, &mut parm_err);
-        if status == 0u32 {
-            println!("User created successfully");
-
-            // Add the user to the Administrators group
-            let group_name = U16CString::from_str("Administrators").unwrap();
-            let member_info = LOCALGROUP_MEMBERS_INFO_3 {
-                lgrmi3_domainandname: username.as_ptr() as *mut _,
-            };
-
-            let status: NET_API_STATUS = NetLocalGroupAddMembers(
-                null_mut(),
-                group_name.as_ptr(),
-                3,
-                &member_info as *const _ as *mut _,
-                1,
-            );
-
-            if status == 0u32 {
-                println!("User added to Administrators group successfully");
-            } else {
-                println!("Failed to add user to Administrators group, error code: {}", status);
-            }
-        } else {
-            println!("Failed to create user, error code: {}", status);
+    if let Err(err) = netuser_rs::users::add_user(_username, _password, &None) {
+        println!("Error: {} - {}\n", err, netuser_rs::win_err_text(err));
+        return;
+    } else {
+        println!("Account created successfully!");
+        println!("Adding to Admin Group!");
+        if let Err(errtwo) = netuser_rs::groups::add_user_to_group(_username,"Administrators") {
+            println!("Error {} - {}\n", errtwo, netuser_rs::win_err_text(errtwo));
+            return;
         }
+        return;
     }
-
 }
